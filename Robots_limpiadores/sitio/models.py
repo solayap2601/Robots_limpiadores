7f7940f693af96a5a0ccbad7b198cadadaed4def
solayap2601 <solayap@unal.edu.co>
2023-11-27 00:15:12 -0500

    modelos

--- Robots_limpiadores/sitio/models.py
+++ Robots_limpiadores/sitio/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Plataforma(models.Model):
+    pass  # No necesitamos definir los campos de robots y habitaciones aquí
+
+class Robot(models.Model):
+    ESTADOS = (
+        ('1', 'Disponible'),
+        ('2', 'Ocupado'),
+    )
+    id = models.AutoField(primary_key=True)
+    estado = models.CharField(max_length=1, choices=ESTADOS)
+    ubicacion = models.ForeignKey('Habitacion', on_delete=models.SET_NULL, null=True)
+    bateria = models.IntegerField()
+    mapa = models.TextField()  # Esto podría ser un campo JSON, dependiendo de cómo quieras manejarlo
+    plataforma = models.ForeignKey('Plataforma', on_delete=models.CASCADE)  # Aquí definimos la relación con Plataforma
+
+class EstacionDeCarga(models.Model):
+    ubicacion = models.CharField(max_length=255)
+    capacidad = models.IntegerField()
+    robots = models.ManyToManyField('Robot')
+
+class Habitacion(models.Model):
+    ESTADOS = (
+        ('1', 'Limpio'),
+        ('2', 'Sucio'),
+    )
+    TIPOS = (
+        ('1', 'Individual'),
+        ('2', 'Doble'),
+        # Agrega más tipos según sea necesario
+    )
+    numero = models.CharField(max_length=10)
+    estado = models.CharField(max_length=1, choices=ESTADOS)
+    tipo = models.CharField(max_length=1, choices=TIPOS)
+    plataforma = models.ForeignKey('Plataforma', on_delete=models.CASCADE)  # Aquí definimos la relación con Plataforma
